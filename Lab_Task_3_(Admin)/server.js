3db15ce8cb3c687d90ed9b4a90796ba41b8e3cbf
fahadAziz-byte <sp23-bse-089@cuilahore.edu.pk>
2024-12-03 07:14:53 +0500

    !?

--- Lab_Task_3_(Admin)/server.js
+++ Lab_Task_3_(Admin)/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs=require('fs');
 var expressLayouts = require("express-ejs-layouts");
 let server = express();
 server.set("view engine", "ejs");
@@ -12,9 +13,15 @@ server.use(adminProductsRouter);
 server.get("/about-me", (req, res) => {
   return res.render("about-me");
 });
-server.get("/", (req, res) => {
-  return res.send(res.render("homepage"));
+
+server.get("/", (req, res) =>{
+  res.render("HomePage.ejs");
 });
+
+server.post("/products/save",(req,res)=>{
+  
+})
+
 server.listen(5000, () => {
   console.log(`Server Started at localhost:5000`);
 });
