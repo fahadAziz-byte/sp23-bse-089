f62ef1b31890d54658f695682100472fd3c1d3df
fahadAziz-byte <sp23-bse-089@cuilahore.edu.pk>
2024-12-15 07:02:48 +0500

    Auth File upload -> Ass#4

--- Ass#4/routes/admin/products.controller.js
+++ Ass#4/routes/admin/products.controller.js
@@ -1,6 +1,7 @@
 const express = require("express");
 let router = express.Router();
 let multer = require("multer");
+router.use(express.urlencoded({extended:true}))
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads"); // Directory to store files
@@ -10,22 +11,21 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage: storage });
-let Product = require("../../models/product.model");
+let Product = require("../../model/productModel");
+
+
 
 // route to handle Delete of product
 router.get("/products/delete/:id", async (req, res) => {
-  let params = req.params;
-  let product = await Product.findByIdAndDelete(req.params.id);
-  // let query = req.query;
-  // return res.send({ query, params });
-  // return res.send(product);
-  return res.redirect("/products");
+  await Product.findByIdAndDelete(req.params.id);
+  return res.redirect("/admin/products");
 });
+
 
 //route to render edit product form
 router.get("/products/edit/:id", async (req, res) => {
   let product = await Product.findById(req.params.id);
-  return res.render("admin/product-edit-form", {
+  return res.render("admin/products/product-edit-form.ejs", {
     layout: "adminlayout",
     product,
   });
@@ -36,12 +36,12 @@ router.post("/products/edit/:id", async (req, res) => {
   product.description = req.body.description;
   product.price = req.body.price;
   await product.save();
-  return res.redirect("/products");
+  return res.redirect("/admin/products");
 });
 
 // route to render create product form
 router.get("/products/create", (req, res) => {
-  return res.render("admin/product-form", { layout: "adminlayout" });
+  return res.render("admin/products/product-form", { layout: "adminlayout" });
 });
 
 //route to handle create product form submission
@@ -50,15 +50,14 @@ router.post(
   "/products/create",
   upload.single("file"),
   async (req, res) => {
-    // return res.send(req.file);
     let data = req.body;
     let newProduct = new Product(data);
-    newProduct.title = data.title;
+    console.log(req.file);
     if (req.file) {
       newProduct.picture = req.file.filename;
     }
     await newProduct.save();
-    return res.redirect("/products");
+    return res.redirect("/admin/products");
     // we will send data to model to save in db
 
     // return res.send(newProduct);
--- Ass#4/server.js
+++ Ass#4/server.js
@@ -1,12 +1,49 @@
 const express = require("express");
 var expressLayouts = require("express-ejs-layouts");
+const mongoose=require('mongoose');
+let cookieParser = require("cookie-parser");
+let session = require("express-session");
+let User=require('./model/User')
 let server = express();
+server.use(express.urlencoded({extended:true}));
 server.set("view engine", "ejs");
 server.use(expressLayouts);
 server.use(express.static("public"));
+server.use(express.static('uploads'));
+server.use(cookieParser());
 
-let adminProductsRouter = require("./routes/admin/products.controller");
+let adminProductsRouter = require("../Ass#4/routes/admin/products.controller");
 server.use('/admin',adminProductsRouter);
+
+server.use(session({ secret: "my session secret" }));
+
+server.get("/logout", async (req, res) => {
+  req.session.user = null;
+  return res.redirect("/login");
+});
+server.get("/login", async (req, res) => {
+  return res.render("auth/login");
+});
+server.post("/login", async (req, res) => {
+  let data = req.body;
+  let user = await User.findOne({ email: data.email });
+  if (!user) return res.redirect("/register");
+  isValid = user.password == data.password;
+  if (!isValid) return res.redirect("/login");
+  req.session.user = user;
+  return res.redirect("/");
+});
+server.get("/register", async (req, res) => {
+  return res.render("auth/register");
+});
+server.post("/register", async (req, res) => {
+  let data = req.body;
+  let user = await User.findOne({ email: data.email });
+  if (user) return res.redirect("/register");
+  user = new User(data);
+  await user.save();
+  return res.redirect("/login");
+});
 
 
 server.get("/about-me", (req, res) => {
@@ -17,10 +54,14 @@ server.get("/", (req, res) =>{
   res.render("HomePage.ejs");
 });
 
-server.post("/products/save",(req,res)=>{
-  
-})
+
 
 server.listen(5000, () => {
   console.log(`Server Started at localhost:5000`);
 });
+
+let connectionString = "mongodb://localhost/sp23-bse-b";
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to Mongo DB Server: " + connectionString))
+  .catch((error) => console.log(error.message));
